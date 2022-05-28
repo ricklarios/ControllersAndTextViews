dc9fd658b2be3b1c4ee63cb49d255cafb9ee2da7
ricklarios <81334604+ricklarios@users.noreply.github.com>
2022-05-28 23:34:36 +0200

    Slider controller + exercise

--- ControllersAndTextViews/ViewController.swift
+++ ControllersAndTextViews/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
 	@IBOutlet weak var myPageControl: UIPageControl!
 	@IBOutlet weak var mySegmentedControl: UISegmentedControl!
 	
+	@IBOutlet weak var mySlider: UISlider!
 	// Variables
 	
 	private let myPickerViewValues = ["Uno", "Dos", "Tres", "Cuatro", "Cinco"]
@@ -48,10 +49,18 @@ class ViewController: UIViewController {
 		for (index, value) in myPickerViewValues.enumerated() {
 			mySegmentedControl.insertSegment(withTitle: value, at: index, animated: true)
 		}
+		
+		// Slider
+		
+		mySlider.minimumTrackTintColor = .red
+		mySlider.minimumValue = 1
+		mySlider.maximumValue = Float(myPickerViewValues.count)
+		mySlider.value = 1
 	}
 
 	// Actions
-
+	
+	// Buttom Actions
 	@IBAction func myButtonAction(_ sender: Any) {
 		if myButton.backgroundColor == .blue {
 			myButton.backgroundColor = .green
@@ -60,6 +69,7 @@ class ViewController: UIViewController {
 		}
 	}
 	
+	// PageControl Actions
 	@IBAction func myPageControlAction(_ sender: Any) {
 		myPickerView.selectRow(myPageControl.currentPage, inComponent: 0, animated: true)
 		
@@ -67,8 +77,10 @@ class ViewController: UIViewController {
 		myButton.setTitle(myString, for: .normal)
 		
 		mySegmentedControl.selectedSegmentIndex = myPageControl.currentPage
+		mySlider.value = Float(myPageControl.currentPage + 1)
 	}
 	
+	// SegmentControl Actions
 	@IBAction func mySegmentControlAction(_ sender: Any) {
 		myPickerView.selectRow(mySegmentedControl.selectedSegmentIndex, inComponent: 0, animated: true)
 		
@@ -76,7 +88,34 @@ class ViewController: UIViewController {
 		myButton.setTitle(myString, for: .normal)
 		
 		myPageControl.currentPage = mySegmentedControl.selectedSegmentIndex
+		mySlider.value = Float(mySegmentedControl.selectedSegmentIndex + 1)
 	}
+	
+	// Slider Actions
+	
+	@IBAction func mySliderAction(_ sender: Any) {
+		
+		// print(mySlider.value)
+		
+		switch mySlider.value {
+		case 1..<2:
+			mySegmentedControl.selectedSegmentIndex = 0
+		case 2..<3:
+			mySegmentedControl.selectedSegmentIndex = 1
+		case 3..<4:
+			mySegmentedControl.selectedSegmentIndex = 2
+		case 4..<5:
+			mySegmentedControl.selectedSegmentIndex = 3
+		default:
+			mySegmentedControl.selectedSegmentIndex = 4
+		}
+		myPageControl.currentPage = mySegmentedControl.selectedSegmentIndex
+		myPickerView.selectRow(mySegmentedControl.selectedSegmentIndex, inComponent: 0, animated: true)
+		let myString = myPickerViewValues[mySegmentedControl.selectedSegmentIndex]
+		myButton.setTitle(myString, for: .normal)
+		
+	}
+	
 	
 }
 
@@ -90,10 +129,11 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
 	func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
 		return myPickerViewValues.count
 	}
-	
+	// Título de cada componente
 	func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
 		return myPickerViewValues[row]
 	}
+	// Si selecciona esa fila entonces...
 	func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
 		let myString = myPickerViewValues[row]
 		myButton.setTitle(myString, for: .normal)
@@ -101,6 +141,8 @@ extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate {
 		myPageControl.currentPage = row
 		
 		mySegmentedControl.selectedSegmentIndex = row
+		
+		mySlider.value = Float(row + 1)
 	}
 }
 
